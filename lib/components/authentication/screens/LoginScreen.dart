bd70557f68fe559e5d5792d0b9f347e4e55e30cc
Abhishek Chouhan <abhich1607@gmail.com>
2021-02-07 10:34:37 +0530

    Login screen made responsive

--- lib/components/authentication/screens/LoginScreen.dart
+++ lib/components/authentication/screens/LoginScreen.dart
@@ -12,196 +12,206 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(40),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                this.screenType,
-                style: TextStyle(
-                  fontSize: 50,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(40),
+            constraints: BoxConstraints(
+                minWidth: double.infinity,
+                minHeight: MediaQuery.of(context).size.height -
+                    MediaQuery.of(context).padding.top,
+                maxHeight: double.infinity),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  this.screenType,
+                  style: TextStyle(
+                    fontSize: 50,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Align(
-                alignment: Alignment.center,
-                child: Container(
-                  constraints: BoxConstraints(maxWidth: 500),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 20.0),
-                        child: FadeAnimation(
-                          delay: 1,
-                          child: Text(
-                            'YOUR EMAIL',
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                      FadeAnimation(
-                        delay: 1,
-                        startY: 20,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.grey[100],
-                            contentPadding: EdgeInsets.symmetric(
-                                horizontal: 30, vertical: 20),
-                            focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 30, bottom: 20),
-                        child: FadeAnimation(
-                          delay: 1,
-                          child: Text(
-                            'PASSWORD',
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                      FadeAnimation(
-                        delay: 1,
-                        startY: 20,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.grey[100],
-                            contentPadding: EdgeInsets.symmetric(
-                                horizontal: 30, vertical: 20),
-                            focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: double.maxFinite,
-                        child: Padding(
-                          padding:
-                              const EdgeInsets.only(top: 50.0, bottom: 20.0),
-                          child: Hero(
-                            tag: this.screenType,
-                            child: TextButton(
-                              onPressed: () {},
-                              child: Text(
-                                this.screenType,
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              style: TextButton.styleFrom(
-                                backgroundColor: Colors.green,
-                                padding: EdgeInsets.symmetric(vertical: 20),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.topCenter,
-                        child: FadeAnimation(
-                          delay: 2,
-                          child: TextButton(
-                            onPressed: () => {
-                              Navigator.push(
-                                context,
-                                PageRouteBuilder(
-                                  transitionDuration: Duration(seconds: 2),
-                                  pageBuilder: (_, __, ___) => ForgotPassword(),
-                                ),
-                              )
-                            },
+                Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    padding: EdgeInsets.only(top: 20, bottom: 20),
+                    constraints: BoxConstraints(maxWidth: 500),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 20.0),
+                          child: FadeAnimation(
+                            delay: 1,
                             child: Text(
-                              'Forgot Password?',
+                              'YOUR EMAIL',
                               style: TextStyle(
-                                fontSize: 15,
+                                fontSize: 18,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
+                          ),
+                        ),
+                        FadeAnimation(
+                          delay: 1,
+                          startY: 20,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.grey[100],
+                              contentPadding: EdgeInsets.symmetric(
+                                  horizontal: 30, vertical: 20),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 30, bottom: 20),
+                          child: FadeAnimation(
+                            delay: 1,
+                            child: Text(
+                              'YOUR PASSWORD',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                        FadeAnimation(
+                          delay: 1,
+                          startY: 20,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.grey[100],
+                              contentPadding: EdgeInsets.symmetric(
+                                  horizontal: 30, vertical: 20),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: double.maxFinite,
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.only(top: 50.0, bottom: 20.0),
+                            child: Hero(
+                              tag: this.screenType,
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text(
+                                  this.screenType,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                style: TextButton.styleFrom(
+                                  backgroundColor: Colors.green,
+                                  padding: EdgeInsets.symmetric(vertical: 20),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.topCenter,
+                          child: FadeAnimation(
+                            delay: 2,
+                            child: TextButton(
+                              onPressed: () => {
+                                Navigator.push(
+                                  context,
+                                  PageRouteBuilder(
+                                    transitionDuration: Duration(seconds: 2),
+                                    pageBuilder: (_, __, ___) =>
+                                        ForgotPassword(),
+                                  ),
+                                )
+                              },
+                              child: Text(
+                                'Forgot Password?',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                FadeAnimation(
+                  delay: 2,
+                  child: Row(
+                    children: [
+                      Text(
+                        (() {
+                          if (this.screenType == 'LOG IN') {
+                            return 'Don\'t have an account?  ';
+                          }
+                          return 'Already have an account?  ';
+                        })(),
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () => {
+                          Navigator.pushReplacement(
+                            context,
+                            PageRouteBuilder(
+                              transitionDuration: Duration(seconds: 2),
+                              pageBuilder: (_, __, ___) => LoginScreen(() {
+                                if (this.screenType == 'LOG IN')
+                                  return 'SIGN UP';
+                                return 'LOG IN';
+                              }()),
+                            ),
+                          )
+                        },
+                        child: Text(
+                          (() {
+                            if (this.screenType == 'LOG IN') {
+                              return "Sign Up";
+                            }
+                            return "Log In";
+                          })(),
+                          style: TextStyle(
+                            fontSize: 15,
                           ),
                         ),
                       ),
                     ],
                   ),
                 ),
-              ),
-              FadeAnimation(
-                delay: 2,
-                child: Row(
-                  children: [
-                    Text(
-                      (() {
-                        if (this.screenType == 'LOG IN') {
-                          return 'Don\'t have an account?  ';
-                        }
-                        return 'Already have an account?  ';
-                      })(),
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () => {
-                        Navigator.pushReplacement(
-                          context,
-                          PageRouteBuilder(
-                            transitionDuration: Duration(seconds: 2),
-                            pageBuilder: (_, __, ___) => LoginScreen(() {
-                              if (this.screenType == 'LOG IN') return 'SIGN UP';
-                              return 'LOG IN';
-                            }()),
-                          ),
-                        )
-                      },
-                      child: Text(
-                        (() {
-                          if (this.screenType == 'LOG IN') {
-                            return "Sign Up";
-                          }
-                          return "Log In";
-                        })(),
-                        style: TextStyle(
-                          fontSize: 15,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
